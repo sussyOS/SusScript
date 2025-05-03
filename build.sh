49abb3694256f8311a3d72d186fd07fc3194c53f
George Biskas Archaniotakis <georgebiskasarchaniotakis@gmail.com>
2025-05-03 20:47:37 +0300

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,18 @@
+@echo off
+@mkdir dist >> /dev/null
+@set osbuild=%1
+@clear
+if osbuild==W (
+    @nuitka --mode="onefile" --no-deployment-flag=self-execution --output-dir=dist sussy.py
+    @nuitka --mode="onefile" --output-dir=dist vent.py
+    @copy /y dist\sussy.exe C:\Users\George\Documents\sus\bin\sussy.exe >> /dev/null
+    @copy /y dist\vent.exe C:\Users\George\Documents\sus\bin\vent.exe >> /dev/null
+    @rmdir /s /q dist
+) else if osbuild==M (
+    exit
+) else if osbuild==L (
+    exit
+) else (
+    @echo Invalid OS build option. Please choose W, M, or L.
+    exit /b 1
+)
