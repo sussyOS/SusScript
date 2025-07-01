ca07bca2f9aa8e5137c1b01a85984205f21f51a6
George Biskas Archaniotakis <georgebiskasarchaniotakis@gmail.com>
2025-07-01 19:26:36 +0300

    finally added linux support(on ubuntu NOT TESTED)!!!!

--- build.bat
+++ build.bat
@@ -11,7 +11,11 @@ if %osbuild%==W (
 ) else if %osbuild%==M (
     exit
 ) else if %osbuild%==L (
-    exit
+    @nuitka --mode="onefile" --no-deployment-flag=self-execution --output-dir=dist sussy.py
+    @nuitka --mode="onefile" --output-dir=dist vent.py
+    @copy /y dist\sussy.exe C:\Users\George\Documents\sus\bin\sussy.exe >> nul 
+    @copy /y dist\vent.exe C:\Users\George\Documents\sus\bin\vent.exe >> nul
+    @rmdir /s /q dist
 ) else (
     @echo Invalid OS build option. Please choose W, M, or L.
     exit /b 1
--- build.sh
+++ build.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
+osbuild=$1
+
 mkdir -p dist
-osbuild="$1"
 clear
 
 if [ "$osbuild" = "W" ]; then
-    nuitka --mode="onefile" --no-deployment-flag=self-execution --output-dir=dist sussy.py
-    nuitka --mode="onefile" --output-dir=dist vent.py
-    cp -f dist/sussy.exe "/mnt/c/Users/George/Documents/sus/bin/sussy.exe"
-    cp -f dist/vent.exe "/mnt/c/Users/George/Documents/sus/bin/vent.exe"
+    nuitka --mode=onefile --no-deployment-flag=self-execution --output-dir=dist sussy.py
+    nuitka --mode=onefile --output-dir=dist vent.py
+    cp -f dist/sussy.exe ~/Documents/sus/bin/sussy.exe
+    cp -f dist/vent.exe ~/Documents/sus/bin/vent.exe
     rm -rf dist
 elif [ "$osbuild" = "M" ]; then
     exit 0
 elif [ "$osbuild" = "L" ]; then
-    exit 0
+    nuitka --mode=onefile --output-filename=sussy --no-deployment-flag=self-execution --output-dir=dist sussy.py
+    nuitka --mode=onefile --output-filename=vent --output-dir=dist vent.py
+    cp -f dist/sussy ~/Documents/sus/bin/sussy
+    cp -f dist/vent ~/Documents/sus/bin/vent
+    rm -rf dist
 else
     echo "Invalid OS build option. Please choose W, M, or L."
     exit 1
